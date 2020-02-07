278a6d1382b658ca5b73675ab808c17f9a45655c
Rachel Schieferstein <violenza@gmail.com>
2020-02-07 10:01:43 -0800

    created class object constructor for age stats

---
+++ src/calc.js
@@ -0,0 +1,14 @@
+export class Ager {
+  constructor(){
+    this.earthAge
+    this.mercuryAge
+    this.venusAge
+    this.marsAge
+    this.jupiterAge
+    this.earthExpectancy
+    this.mercuryRemain
+    this.venusRemain
+    this.marsRemain
+    this.jupiterRemain
+  }
+}
