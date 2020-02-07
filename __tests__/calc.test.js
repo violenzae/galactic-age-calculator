92e2f78265b459ece4fadff22e5154b409712ad9
Rachel Schieferstein <violenza@gmail.com>
2020-02-07 10:33:52 -0800

    mercury test passed

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -3,7 +3,8 @@ import {Ager} from './../src/calc.js';
 describe('Ager', () => {
 
   test('returns age in mercury years', () => {
-  let newAger = new Ager;
+  let newAger = new Ager(32);
+  newAger.mercuryAge = Math.floor(newAger.earthAge/.24);
   
   expect(newAger.mercuryAge).toEqual(133);
   });
