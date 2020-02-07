df5613036ef9e10529ce1f2018a369809aab10d7
Rachel Schieferstein <violenza@gmail.com>
2020-02-07 14:46:41 -0800

    adding bootstrap styling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 import { Ager } from './calc';
 
 $(document).ready(function() {
