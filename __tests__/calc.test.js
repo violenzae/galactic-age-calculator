5eef477fabc27bd7ed6f317687783e19210f79f1
Rachel Schieferstein <violenza@gmail.com>
2020-02-07 10:58:36 -0800

    adding failed venus test

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -1,11 +1,21 @@
 import {Ager} from './../src/calc.js';
 
-describe('Ager', () => {
+describe('Mercury convert', () => {
 
   test('returns age in mercury years', () => {
   let newAger = new Ager("Mercury", 32, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  
   newAger.baseAgeConvert();
   
   expect(newAger.mercuryAge).toEqual(133);
+  });
+});
+
+describe('venus convert', () => {
+
+  test('returns age in venus years', () => {
+  let newAger = new Ager("Venus", 32)
+  newAger.venusAge = Math.floor(this.earthAge/.62);
+  expect(newAger.venusAge).toEqual(51);
   });
 });
