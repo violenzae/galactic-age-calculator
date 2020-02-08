ae891df1eee58981b26020d4996d6e532d6acd2b
Rachel Schieferstein <violenza@gmail.com>
2020-02-07 16:44:39 -0800

    finished basic functional UI

--- src/main.js
+++ src/main.js
@@ -5,10 +5,42 @@ import './styles.css';
 import { Ager } from './calc';
 
 $(document).ready(function() {
+  $("form#planetage").submit(function(event) {
+    event.preventDefault();
 
-  let newAger = new Ager("Mercury", 90, "Female", "White", "Bachelors", 0, 84);
+    const gender = $("#gender").val();
+    const race = $("#race").val();
+    const education = $("#education").val();
+    const planet = $("#planet").val();
+    const earthAge = parseInt($("#age").val());    
 
-  newAger.remainConvert();
+    let newAger = new Ager(planet, earthAge, gender, race, education);
 
+    const planetAge = newAger.baseAgeConvert();
+    const earthExpectancy = newAger.expectancyEarthTotaler();
+    const planetRemain = newAger.remainConvert();
+    const overExpectancy = newAger.excessYearToPos();
 
+    $("#years-over").hide();
+    $("#years-left").hide();
+
+    $(".results-col").show();
+    $("#planet-name").text(planet);
+    $("#age-entered").text(earthAge);
+    $("#planet-age").text(planetAge);
+    $("#earth-expectancy").text(earthExpectancy)
+
+    $("#planet-remain").text(planetRemain); 
+    $("#planet-over").text(overExpectancy);
+
+    if (planetRemain >= 0) {
+      $("#years-left").show();
+    } else { 
+      $("#years-over").show();
+    }
+
+    $("#planet-remain").text(planetRemain); 
+    $("#planet-over").text(overExpectancy);
+    
+  });
 });
